76e3eb7deb676904bf06bf3a48b7b9baed541e3c
Viken Manoukian <68619882+string-dot-byte@users.noreply.github.com>
2021-05-25 08:08:27 -0400

    How the hell am I supposed to format this

--- hello.lua
+++ hello.lua
@@ -2,11 +2,11 @@ function RunHello(str)
     local res = ''
     for char in str:gmatch('.') do
         if char == 'h' then
-			res = res .. 'Hello World'
-		else
+		res = res .. 'Hello World'
+	else
             res = 'ERROR!'
             break
-		end
+	end
 	end
     
 	return res
